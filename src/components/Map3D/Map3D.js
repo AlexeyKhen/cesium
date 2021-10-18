589b910278a344633b5f6b6a7a3958f88c55c1dd
Alexey <67691338+AlexeyKhen@users.noreply.github.com>
2021-10-18 16:37:09 +0600

    123

--- src/components/Map3D/Map3D.js
+++ src/components/Map3D/Map3D.js
@@ -1,15 +1,16 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Cesium3DTileset, Viewer as ViewerResium} from "resium";
 import {Cartesian3, OpenStreetMapImageryProvider} from "cesium";
 import Load3DModels from "../Files3D/Files3D";
 
 function Map3D() {
+    const [isLoading, setLoading] = useState(true)
     const ref = useRef(null);
     const initialImageryProvider3D = new OpenStreetMapImageryProvider({
         url: "https://a.tile.openstreetmap.org/",
     });
 
-    useEffect(()=>{
+    useEffect(() => {
         if (ref.current && ref.current.cesiumElement) {
             let viewer = ref.current.cesiumElement;
             viewer._container.firstChild.children[1].style.display = "none";
@@ -22,39 +23,44 @@ function Map3D() {
             })
             const eventLoaded = (tiles) => {
 
-                if (tiles === 0) {console.log('finished')
+                if (tiles === 0) {
+                    setLoading(false)
+                    console.log('loaded')
                 }
             };
             const loader = viewer.scene.globe.tileLoadProgressEvent;
             loader.addEventListener(eventLoaded);
         }
 
-    },[
-
-    ])
+    }, [])
 
 
     return (
-        <ViewerResium
-            ref={ref}
-            homeButton={false}
-            // imageryProvider={initialImageryProvider3D}
-            navigationHelpButton={false}
-            geocoder={false}
-            fullscreenButton={false}
-            timeline={false}
-            sceneModePicker={false}
-            baseLayerPicker={false}
-            vrButton={false}
-            selectionIndicator={false}
-            infoBox={false}
-            animation={false}
-            style={{height: 'calc(100vh)'}}
-        >
-            {/*<Cesium3DTileset url="http://localhost:8000/media/sector107/Cesium_107_1_Cesium.json" />*/}
-            <Load3DModels/>
-        </ViewerResium>
-    );
+        <>
+            {isLoading ? <img src={'load.gif'} style={{height: 'calc(100vh)', width: 'calc(100vw)'}}/> : null}
+            <div style={isLoading ? {visibility: 'hidden'} : null}>
+                <ViewerResium
+                    ref={ref}
+                    homeButton={false}
+                    // imageryProvider={initialImageryProvider3D}
+                    navigationHelpButton={false}
+                    geocoder={false}
+                    fullscreenButton={false}
+                    timeline={false}
+                    sceneModePicker={false}
+                    baseLayerPicker={false}
+                    vrButton={false}
+                    selectionIndicator={false}
+                    infoBox={false}
+                    animation={false}
+                    style={{height: 'calc(100vh)'}}
+                >
+                    {/*<Load3DModels/>*/}
+                </ViewerResium>
+            </div>
+        </>
+    )
+        ;
 }
 
 export default Map3D;
